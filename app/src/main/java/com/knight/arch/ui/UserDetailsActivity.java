21fc5197ba6182c63832383551340db465e76190
andyiac <andyiaczhang@gmail.com>
2015-09-13 00:21:38 +0800

    user detail title bar

--- app/src/main/java/com/knight/arch/ui/UserDetailsActivity.java
+++ app/src/main/java/com/knight/arch/ui/UserDetailsActivity.java
@@ -1,5 +1,9 @@
 package com.knight.arch.ui;
 
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
+
 import com.knight.arch.R;
 import com.knight.arch.module.UserDetailsModule;
 import com.knight.arch.ui.base.InjectableActivity;
@@ -18,6 +22,23 @@ public class UserDetailsActivity extends InjectableActivity {
 
     @Override
     public List<Object> getModules() {
+
         return Arrays.<Object>asList(new UserDetailsModule(this));
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Test!!!");
     }
 }
