446b402f091825f06cf11418436fd9b7d2a9de86
andyiac <andyiaczhang@gmail.com>
2015-09-16 10:19:33 +0800

    ripple view

--- app/src/main/java/com/knight/arch/adapter/GitHubUserRankListAdapterHolder.java
+++ app/src/main/java/com/knight/arch/adapter/GitHubUserRankListAdapterHolder.java
@@ -25,6 +25,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.balysv.materialripple.MaterialRippleLayout;
 import com.knight.arch.R;
 import com.knight.arch.model.User;
 import com.squareup.picasso.Picasso;
@@ -49,6 +50,15 @@ public class GitHubUserRankListAdapterHolder extends RecyclerView.Adapter<GitHub
         final LayoutInflater mInflater = LayoutInflater.from(parent.getContext());
         final View sView = mInflater.inflate(R.layout.github_user_rank_single_list_item, parent, false);
         return new ViewHolder(sView);
+
+//        return new ViewHolder(
+//                MaterialRippleLayout.on(mInflater.inflate(R.layout.github_user_rank_single_list_item, parent, false))
+//                        .rippleOverlay(true)
+//                        .rippleAlpha(0.2f)
+//                        .rippleColor(0xFF585858)
+//                        .rippleHover(true)
+//                        .create()
+//        );
     }
 
     @Override
