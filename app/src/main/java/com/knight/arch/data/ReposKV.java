eac1488e6c1bc5a09f44b5ee1c44a4062e2da16f
andyiac <andyiaczhang@gmail.com>
2015-09-20 23:12:09 +0800

    repos details

---
+++ app/src/main/java/com/knight/arch/data/ReposKV.java
@@ -0,0 +1,37 @@
+package com.knight.arch.data;
+
+/**
+ * for repos details
+ *
+ * @author andyiac
+ * @date 15-9-20
+ * @web http://blog.andyiac.com
+ * @github https://github.com/andyiac
+ */
+
+
+public class ReposKV {
+    private String key;
+    private String value;
+
+    public ReposKV(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- app/src/main/java/com/knight/arch/ui/ReposDetailsActivity.java
+++ app/src/main/java/com/knight/arch/ui/ReposDetailsActivity.java
@@ -2,24 +2,34 @@ package com.knight.arch.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.alibaba.fastjson.JSON;
 import com.knight.arch.R;
+import com.knight.arch.adapter.HotReposDetailsListAdapterHolder;
+import com.knight.arch.data.ReposKV;
+import com.knight.arch.model.Repository;
 import com.knight.arch.module.ReposDetailsModule;
 import com.knight.arch.ui.base.InjectableActivity;
 import com.knight.arch.utils.L;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 public class ReposDetailsActivity extends InjectableActivity {
-    private Parcelable mRepository;
+    private Repository mRepository;
+    private RecyclerView mRecyclerView;
+    private HotReposDetailsListAdapterHolder adapter;
+    private LinearLayoutManager linearLayoutManager;
+    private List<ReposKV> mReposData = new ArrayList<>();
 
     @Override
     protected int provideContentViewId() {
-        return R.layout.activity_user_details;
+        return R.layout.activity_repos_details;
     }
 
     @Override
@@ -33,10 +43,26 @@ public class ReposDetailsActivity extends InjectableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        initView();
         Intent intent = getIntent();
         mRepository = intent.getParcelableExtra("repos_data");
         L.json(JSON.toJSONString(mRepository));
+
+        if (mRepository != null) {
+            initData();
+        }
+
+        initView();
+
+    }
+
+    private void initData() {
+        mReposData.add(new ReposKV("name", mRepository.getName()));
+        mReposData.add(new ReposKV("full_name", mRepository.getFull_name()));
+        mReposData.add(new ReposKV("description", mRepository.getDescription()));
+        mReposData.add(new ReposKV("create_at", mRepository.getCreated_at()));
+        mReposData.add(new ReposKV("update_at", mRepository.getUpdated_at()));
+        mReposData.add(new ReposKV("stars", String.valueOf(mRepository.getStargazers_count())));
+        mReposData.add(new ReposKV("language", String.valueOf(mRepository.getLanguage())));
     }
 
     private void initView() {
@@ -45,6 +71,14 @@ public class ReposDetailsActivity extends InjectableActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbarLayout.setTitle("Test!!!");
+        collapsingToolbarLayout.setTitle(mRepository.getName());
+
+
+        adapter = new HotReposDetailsListAdapterHolder(this, mReposData);
+        mRecyclerView = (RecyclerView) findViewById(R.id.id_repos_details_recycler_view);
+        mRecyclerView.setAdapter(adapter);
+        linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
     }
 }
