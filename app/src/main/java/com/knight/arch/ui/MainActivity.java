c1cd3b9b9cf9b45a625c122a40c7f5e227127d29
andyiac <andyiaczhang@gmail.com>
2015-09-22 18:32:58 +0800

    system bar marging

--- app/src/main/java/com/knight/arch/ui/MainActivity.java
+++ app/src/main/java/com/knight/arch/ui/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
