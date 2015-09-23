ee79f1b4178184d2a8fe98410b77b79ac9db6f96
andyiac <andyiaczhang@gmail.com>
2015-09-23 18:21:05 +0800

    licence

--- app/src/main/java/com/knight/arch/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/knight/arch/ui/fragment/SettingsFragment.java
@@ -2,10 +2,13 @@ package com.knight.arch.ui.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import com.knight.arch.R;
 import com.knight.arch.module.Injector;
+
+import de.psdev.licensesdialog.LicensesDialog;
 
 
 /**
@@ -24,6 +27,20 @@ public class SettingsFragment extends PreferenceFragment {
         getActivity();
         addPreferencesFromResource(R.xml.preference);
         findPreference(getString(R.string.pref_build_time)).setSummary("2015年08月15日");
+        findPreference(getString(R.string.open_source_licence)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                new LicensesDialog.Builder(getActivity())
+                        .setNotices(R.raw.notices)
+                        .setIncludeOwnLicense(true)
+                        .setThemeResourceId(R.style.custom_theme)
+//                        .setDividerColorId(R.color.dialog_custom_divider_color)
+                        .build()
+                        .show();
+                return false;
+            }
+        });
+
     }
 
     @Override
