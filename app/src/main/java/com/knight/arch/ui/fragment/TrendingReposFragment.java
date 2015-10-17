9af2a8ef9793d22b7b1404b12a3495e1a9b6dd0f
andyiac <andyiaczhang@gmail.com>
2015-10-17 14:52:52 +0800

    修改API文档

---
+++ app/src/main/java/com/knight/arch/ui/fragment/TrendingReposFragment.java
@@ -0,0 +1,111 @@
+package com.knight.arch.ui.fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.knight.arch.R;
+import com.knight.arch.ui.base.BaseFragment;
+import com.umeng.analytics.MobclickAgent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author andyiac
+ * @date 15-10-16
+ * @web http://blog.andyiac.com
+ * @github https://github.com/andyiac
+ */
+@SuppressLint("ValidFragment")
+public class TrendingReposFragment extends BaseFragment {
+
+    private TabLayout mTabLayout;
+
+    public TrendingReposFragment() {
+    }
+
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onPageStart("TrendingReposFragment"); //统计页面
+    }
+
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPageEnd("TrendingReposFragment");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        setStatusColor(android.R.color.transparent);
+        View view = inflater.inflate(R.layout.fragment_hot_repos_main2, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.hot_repos_fragment_viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        mTabLayout = (TabLayout) view.findViewById(R.id.hot_repos_tabs);
+        mTabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        if (viewPager != null) {
+            mTabLayout.setupWithViewPager(viewPager);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new RankReposFragment("language:Java"), "Java");
+        adapter.addFragment(new RankReposFragment("language:C"), "C");
+        adapter.addFragment(new RankReposFragment("language:Objective-C"), "Objective-C");
+        adapter.addFragment(new RankReposFragment("language:csharp"), "C#");
+        adapter.addFragment(new RankReposFragment("language:Python"), "Python");
+        adapter.addFragment(new RankReposFragment("language:PHP"), "PHP");
+        adapter.addFragment(new RankReposFragment("language:JavaScript"), "JavaScript");
+        adapter.addFragment(new RankReposFragment("language:Ruby"), "Ruby");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            return mFragmentTitles.get(position);
+        }
+    }
+}
---
+++ docs/GitHotAPI.md
@@ -0,0 +1,64 @@
+# GitHot API
+
+## Repos
+
+- repos 排序使用API
+	
+	按照Java,C,Objective-C,csharp,Python,PHP,JavaScript,Ruby进行分类
+	
+	github默认按照start量进行排序，各个语言排序接口如下：
+
+		https://api.github.com/search/repositories?q=language:Java&page=1
+		https://api.github.com/search/repositories?q=language:C&page=1
+		https://api.github.com/search/repositories?q=language:Objective-C&page=1
+		https://api.github.com/search/repositories?q=language:csharp&page=1
+		https://api.github.com/search/repositories?q=language:Python&page=1
+		https://api.github.com/search/repositories?q=language:PHP&page=1
+		https://api.github.com/search/repositories?q=language:JavaScript&page=1
+		https://api.github.com/search/repositories?q=language:Ruby&page=1
+	注意：
+	
+	repository的详细信息和repository的owner信息都包含在此API返回的数据中。
+
+## Trending repos
+
+- trending repos 使用了第三方的一个API
+
+		http://trending.codehub-app.com/trending
+		
+- trending repos 跟github 一样trending 是有三个
+
+		http://trending.codehub-app.com/v2/trending?language=c&since=daily
+		http://trending.codehub-app.com/v2/trending?language=c&since=weekly
+		http://trending.codehub-app.com/v2/trending?language=c&since=monthly
+		
+
+## Users
+- users 排序使用API
+
+	按照Java,C,Objective-C,csharp,Python,PHP,JavaScript,Ruby进行分类
+
+		https://api.github.com/search/users?q=location:china&page=1
+		https://api.github.com/search/users?q=language:Java+followers:>500&page=1
+		https://api.github.com/search/users?q=language:C+followers:>500&page=1
+		...
+	注意：
+	
+	users排序比repos多加了一个按照地域location:china的分类，放在了第一项
+	
+		https://api.github.com/search/users?q=location:china&page=1
+	
+	另外，q=queryParams 中的queryParams需要进行URLencoding 如：
+	
+		https://api.github.com/search/users?q=language:Java+followers:%3E500&page=1
+	
+- 某一个用户信息,比如如andyiac的详细详细：
+
+		https://api.github.com/users/andyiac/repos
+
+## check update 
+- 检查更新是通过使用 fir.im 提供的更新接口
+
+		http://api.fir.im/apps/latest/55fd648a00fc7453bd000002?api_token=e9643ae1e41c57d6d829504ce8f9f90a
+
+
